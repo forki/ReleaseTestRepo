8618d563289b78e08bda065f33e49a3bd79967c7
Robin Neatherway <robin.neatherway@gmail.com>
2015-07-06 23:29:11 +0100

    1st

---
+++ src/ReleaseTestRepo/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("ReleaseTestRepo")>]
+[<assembly: AssemblyProductAttribute("ReleaseTestRepo")>]
+[<assembly: AssemblyDescriptionAttribute("Just a repo to test releasing")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
